54e9c222a296f0723ddb41f5baf505f37c3d5259
Piet <piet.broemmel@gmail.com>
2021-11-28 13:16:15 +0100

    updated random setting function

--- factorioBlueprintVisualizer/__init__.py
+++ factorioBlueprintVisualizer/__init__.py
@@ -1,7 +1,7 @@
 from .util import get_settings_from_svg, get_blueprint_from_svg, pretty_print_settings, save_svg, print_blueprint_entity_count
 from .building_settings import BUILDING_GENERIC_TERMS, BUILDING_SIZES
 from .example_data import EXAMPLE_SETTINGS, EXAMPLE_BLUEPRINT
-from .visuzalizer import draw_blueprints, get_blueprint_cache, get_custom_building_settings
+from .visuzalizer import draw_blueprint, get_blueprint_cache, get_custom_building_settings
 from .random_settings import get_random_settings, settings_change_colors, settings_change_property
 
 
